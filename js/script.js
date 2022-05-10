789c688fbd97244599a45012b00b56e706b17878
Roman <starsetphenomenon@gmail.com>
2022-05-10 15:18:35 +0300

    promo logo added + responsive fixes

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ document.addEventListener('scroll', function () {
   const szazalek = document.querySelectorAll('.percent__value'),
     lines = document.querySelectorAll('.percent__area');
 
-// Percent animation if it is in a view field and if it's not.
+  // Percent animation if it is in a view field and if it's not.
 
   if (isInViewport(percentBlock)) {
 
@@ -192,3 +192,34 @@ window.addEventListener('scroll', function() {
 
   parallaxButton.style.right = -value * 0.1 + 'px';
 }); */
+
+
+
+// Promo Illustration MOUSE EFFECT
+
+/* let constrain = 20;
+let mouseOverContainer = document.getElementById("ex1");
+let ex1Layer = document.getElementById("ex1-layer");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
+
+  return "perspective(100px) " +
+    "   rotateX(" + calcX + "deg) " +
+    "   rotateY(" + calcY + "deg) ";
+};
+
+function transformElement(el, xyEl) {
+  el.style.transform = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function (e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function () {
+    transformElement(ex1Layer, position);
+  });
+}; */
