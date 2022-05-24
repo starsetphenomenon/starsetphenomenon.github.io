fafe5f32961cc352cc97120aa30588b4a7acd245
Roman <starsetphenomenon@gmail.com>
2022-05-24 09:57:39 +0300

    Active links on scrolling sections added

--- js/script.js
+++ js/script.js
@@ -65,6 +65,28 @@ currentlink.forEach(link => {
   });
 });
 
+// Drop menu links active if scrolls on sections
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".menu__block nav ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(`jsAct__${current}`)) {
+      li.classList.add("active");
+    }
+  });
+};
+
 
 // Smooth scrolling ~~~
 
